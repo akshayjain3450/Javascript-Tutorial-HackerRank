afce0c8157e1950f5cf45fa08acf2ae2ae4c56ca
akshayjain3450 <akshayjain3450@gmail.com>
2020-06-28 21:56:34 +0530

    Day 5: Arrow Functions

---
+++ arrowfunc.js
@@ -0,0 +1,42 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ *
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+    const newarr = nums.map(function (a){if(a % 2 == 0){return a * 2;} else{return a * 3;}}, 0)
+    return newarr
+}
+
+
+function main() {
+    const n = +(readLine());
+    const a = readLine().split(' ').map(Number);
+
+    console.log(modifyArray(a).toString().split(',').join(' '));
+}
